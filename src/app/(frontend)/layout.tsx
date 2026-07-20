e4167caebd4bc82b5a6cb5ebc961ce3e53f2973f
MNSTANCHEV <metody.stanchev@netinsky.com>
2026-07-20 17:27:29 +0300

    Making the favicon

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -50,6 +50,13 @@ export const metadata: Metadata = {
     locale: 'bg_BG',
     type: 'website',
   },
+  icons: {
+    icon: [
+      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
 }
 
 export default async function FrontendLayout({ children }: { children: React.ReactNode }) {
